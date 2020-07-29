87d1b57bf68b3700cf70ea5f5769bd9fab9f68cd
Marcelo Augusto Salomão Ganem <masganem@gmail.com>
2020-07-29 12:24:22 -0300

    Ajustes de Front End

--- projeto_base/ej/views.py
+++ projeto_base/ej/views.py
@@ -1,3 +1,9 @@
+import os
 from flask import render_template, Blueprint, request, redirect, url_for, flash
+from projeto_base.ej.models import Ej
+from projeto_base import db, login_required
+from flask_login import LoginManager, current_user, login_user, logout_user
 
 ej = Blueprint('ej', __name__, template_folder='templates')
+login_manager = LoginManager()
+login_manager.login_view = "principal.index"
