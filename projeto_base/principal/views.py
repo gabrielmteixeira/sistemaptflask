dd2acb56f6327dbadd5aff486cd9d73e19b7c1de
Gabriel Freitas D'aquino Filocre <64698404+GabrielFilocre@users.noreply.github.com>
2020-08-25 12:33:11 -0300

    agora as missões pendentes aparecem na home

--- projeto_base/principal/views.py
+++ projeto_base/principal/views.py
@@ -1,11 +1,16 @@
 from flask import render_template, Blueprint
+from projeto_base.usuario.models import Usuario
+from projeto_base.tarefa.models import Tarefa
+from projeto_base import db
+from flask_login import current_user
 
 principal = Blueprint('principal', __name__, template_folder='templates')
 
 @principal.route('/')
 def index():
-
-    return render_template('tela_principal.html')
+    tarefas = Tarefa.query.all()
+    
+    return render_template('tela_principal.html', tarefas=tarefas)
 
 @principal.route('/dashboard')
 def dashboard():
